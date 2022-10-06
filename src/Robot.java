479d4bfd6cad1c80e8a58116b53768d93254a0cd
Анара Алмазова <anuyka.almaz@gmail.com>
2022-10-06 08:00:09 +0300

    Initial commit

---
+++ src/Robot.java
@@ -0,0 +1,12 @@
+public class Robot {
+    String name;
+    int age;
+    String color;
+    double weight;
+    public void basuu(){
+        System.out.println("Robot basyp jurot");
+    }
+    public void say(){
+        System.out.println("Robot suiloit");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	Robot robot1=new Robot();
+    robot1.name="Tom";
+    robot1.age=3;
+    robot1.color="black";
+    robot1.weight=30.5;
+    robot1.basuu();
+    robot1.say();
+    }
+}
